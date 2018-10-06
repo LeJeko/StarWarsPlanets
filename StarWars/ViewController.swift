44f1ae4af4e33d2c2ac9be7934ed02fcd399fde3
LeJeko <jeko@mac.com>
2018-10-06 12:34:11 +0200

    Initial Commit

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Jérôme Haegeli on 06.10.18.
+//  Copyright © 2018 Jeko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
